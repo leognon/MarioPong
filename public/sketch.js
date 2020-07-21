aaef7fc2db4b0cdfa6ad1a0476968cde5c86d2df
Leo Gagnon <leognon@gmail.com>
2020-07-21 15:17:45 -0400

    Added mobile support

--- public/sketch.js
+++ public/sketch.js
@@ -66,7 +66,22 @@ new p5(() => {
     let deltaTime = 0;
     let lastFrameTime = Date.now();
 
+    let pressing = false;
+    //Credit to https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    window.isMobile = function() {
+        let check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
+
     setup = () => {
+        if (isMobile()) {
+            alert(`Mario Pong works best on a computer. If you cannot use a computer, try using the Google web browser.
+                Be sure you zoom out fully, and refresh the page to reset the elements positioning.`);
+            document.getElementsByClassName('creditsRight')[1].innerText = 'Drag to move your character';
+            document.getElementsByClassName('creditsRight')[2].innerText = 'Fireballs will shoot automatically';
+            document.getElementsByClassName('creditsRight')[3].innerText = 'Mario Pong works best on a computer!';
+        }
         function loaded() {
             amtLoaded++;
             if (amtLoaded >= totalToLoad) { //Once assets have loaded, connect to the server
@@ -142,7 +157,6 @@ new p5(() => {
 
         cnv = createCanvas(origWidth, origHeight);
         joinDiv = select('#joinDiv');
-        joinButton = select('#joinB');
 
         let pName = "Player";
         for (let i = 0; i < 4; i++) pName += floor(random(10));
@@ -154,13 +168,25 @@ new p5(() => {
             if (playerNameInp.value().length < 1) playerNameInp.value(pName);
             localStorage.setItem("MarioPongUsername", playerNameInp.value());
         });
+        playerNameInp.touchStarted(() => {
+            if (playerNameInp.value().length < 1) playerNameInp.value(pName);
+            localStorage.setItem("MarioPongUsername", playerNameInp.value());
+        });
 
         playerNameInp.mouseClicked(() => {
             document.getElementById('playerName').select();
         });
+        playerNameInp.touchStarted(() => {
+            document.getElementById('playerName').select();
+        });
 
         creditsDiv = select('#creditsDiv');
+
+        joinButton = select('#joinB');
         joinButton.mouseClicked(() => {
+            socket.emit('addToQueue', playerNameInp.value()); //Join the queue with the player name
+        });
+        joinButton.touchStarted(() => {
             socket.emit('addToQueue', playerNameInp.value()); //Join the queue with the player name
         });
         widthToHeight = width / height;
@@ -170,6 +196,23 @@ new p5(() => {
     }
 
     draw = () => {
+        if (status == "joined" && player && isMobile()) {
+            if (pressing) {
+                const correctMouse = mouseY / scaleFactor;
+                if (correctMouse < player.pos.y + (player.displayHeight/2)) {
+                    player.up = true;
+                    player.down = false;
+                } else {
+                    player.down = true;
+                    player.up = false;
+                }
+            } else {
+                player.up = false;
+                player.down = false;
+            }
+
+            if (player.powerup == "Fire" && random(1) < 0.01) player.shoot();
+        }
         if (!initialConnect && socket && socket.connected) initialConnect = true;
         if (initialConnect && socket.disconnected) { //If Disconnected, show it and freeze the game
             refreshMsg = "You have been disconnected from the server!\nPlease refresh the page!";
@@ -405,7 +448,6 @@ new p5(() => {
             newHeight = windowWidth / widthToHeight;
         }
 
-
         resizeCanvas(newWidth, newHeight);
         cnv.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2);
 
@@ -418,6 +460,14 @@ new p5(() => {
             player.down = false;
             player.up = false;
         }
+    }
+
+    //Mobile controls
+    window.ontouchstart = () => {
+        pressing = true;
+    }
+    window.ontouchend = () => {
+        pressing = false;
     }
 
     window.onkeydown = k => {
@@ -610,7 +660,6 @@ new p5(() => {
         }
 
     }
-
 
     class MovingSprite {
         constructor(name, w, h, x, y, vx, vy, s, extra) {
